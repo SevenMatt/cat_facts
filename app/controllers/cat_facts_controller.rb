998cbd6643b24c74e001a3e7e2df1fe697913b10
SevenMatt <matheusmamedd@gmail.com>
2025-08-15 14:20:12 -0300

    first commit

--- app/controllers/cat_facts_controller.rb
+++ app/controllers/cat_facts_controller.rb
@@ -4,6 +4,6 @@ class CatFactsController < ApplicationController #=> Cria um controller chamado 
 
   def index #=> Define a ação index do controller. Em Rails, cada ação normalmente corresponde a uma página ou uma resposta.
     response = self.class.get('/fact') #=> Faz a requisição GET para https://catfact.ninja/fact e retorna um JSON com um fato de gato
-   @cat_fact = response.success? && response['fact'].present? ? response['fact'] : "Erro ao buscar fato" #=> @cat_fact -> variável de instância usada na viw. response.success? -> checa se a requisição foi bem sucedida. response['fact'] -> extrai o fato do JSON. Se algo der errado, mostra "Erro ao buscar fato".
+   @cat_fact = response.success? && response['fact'].present? ? response['fact'] : "Erro ao buscar fato" #=> @cat_fact -> variável de instância usada na view. response.success? -> checa se a requisição foi bem sucedida. response['fact'] -> extrai o fato do JSON. Se algo der errado, mostra "Erro ao buscar fato".
   end
 end
